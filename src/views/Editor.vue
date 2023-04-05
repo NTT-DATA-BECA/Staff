4b18acb4eb82dac0320c93894a2f26e01bbc1b06
Chaymae Elmoueden <elmouedenchaimae@gmail.com>
2023-04-05 11:35:05 +0000

    QUILL IMPORT TOPDF

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="m-5">
-    
+    <div class="row">
     <v-select
         v-model="selectedOption"
         :options="files" 
@@ -10,13 +10,17 @@
         @option:selected="onchangeSelect()"        
       ></v-select>  
     <div ref="editor"></div>
+    
     <input type="file" class="mt-5 mr-5 w-30 p-2 " @input="ImportDoc" accept=".doc, .docx">
     <input className="input mr-5" placeholder="Add File name"
                  v-model="fileName"  /> 
     <button class="mt-5 mr-5 w-30 p-2 border-2 border-cyan-600 hover:bg-orange-500 hover:text-white" @click="saveToDatabase">Save</button>
-    <button class="mt-5 w-30 p-2 border-2 border-cyan-600 hover:bg-orange-500 hover:text-white" @click="downloadPdf">Generate PDF</button>
+    <button id="btn" class="mt-5 w-30 p-2 border-2 border-cyan-600 hover:bg-orange-500 hover:text-white" @click="downloadPdf">Generate PDF</button>
+    </div>
+    <iframe id="main-iframe" style="width: 100%; height: 100%; z-index: 2; border: none; margin-top: 40px;"></iframe>
   </div>
- 
+  
+
 </template>
 
 <script lang="ts">
@@ -26,11 +30,11 @@ import ResizeModule from "@ssumo/quill-resize-module";
 //import * as pdfjsLib from "pdfjs-dist/build/pdf"
 import beautify from 'js-beautify'
 import mammoth from 'mammoth/mammoth.browser.js'
-import { jsPDF } from "jspdf"
-
+import jsPDF from 'jspdf'
 import { ipcRenderer } from 'electron';
-
+//import html2pdf from "html2pdf.js";
 Quill.register("modules/resize", ResizeModule);
+
 //pdfjsLib.GlobalWorkerOptions.workerSrc = '../../node_modules/pdfjs-dist/build/pdf.worker.js'
 export default {
   inject: ['ipcRenderer'],
@@ -60,8 +64,7 @@ export default {
       [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
       [{ 'font': [] }],
       [{ 'align': [] }],
-
-      ['clean']                                         // remove formatting button
+      ['clean']  ,  // remove formatting button
     ];
     this.editor = new Quill(this.$refs.editor, {
       theme: 'snow', 
@@ -93,22 +96,28 @@ export default {
         httml.replace(/\u00A0/g, ' '));
       this.editor.root.innerHTML=html;
     },
-   async downloadPdf(){
-    var doc = new jsPDF();
-    doc.html(this.editor.root.innerHTML, {
-      callback: function(doc) {
-            // Save the PDF
-            doc.save('document-html.pdf');
-        },
-        margin: [3, 3, 3, 3],
-        autoPaging: 'text',
-        x: 0,
-        y: 0,
-        width: 190, //target width in the PDF document
-        windowWidth: 775 //window width in CSS pixels
-});
-    },
-
+    async downloadPdf(){
+      var doc = new jsPDF();
+          var name = this.selectedOption
+          var htmleditor = this.editor.root.innerHTML;
+          htmleditor = htmleditor.replace(/>\s+|\s+</g, function(match) {
+            return match.includes('>') ? '>' + '&nbsp;'.repeat(match.length-2) + ' ' : ' ' + '&nbsp;'.repeat(match.length-2) + '<'; 
+          }); 
+          doc.html(htmleditor, {
+            callback: function(doc) {
+                  // Save the PDF
+                  doc.save(name+'.pdf');
+              },
+              margin: [3, 3, 3, 3],
+              autoPaging: 'text',
+              x: 0,
+              y: 0,
+              width: 190, 
+              windowWidth: 1200 ,
+      });  
+   // document.getElementById('main-iframe')?.setAttribute('src', doc.output('datauristring'));
+    }
+   ,
     saveToDatabase(){
   ipcRenderer.invoke('insertQuillcontent', { name: this.fileName, data: this.editor.root.innerHTML })
     .catch((error) => {
