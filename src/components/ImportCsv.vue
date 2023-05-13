1bfa2bfd080c987a6406d8faecfd804f3274b761
Chaymae Elmoueden <elmouedenchaimae@gmail.com>
2023-05-13 11:58:10 +0100

    Converted code to use the data/Mounted structure

--- src/components/ImportCsv.vue
+++ src/components/ImportCsv.vue
@@ -1,93 +1,82 @@
 <template>
-<div ref="el" class="input_field flex flex-col w-max mx-auto text-center">
-     <label >
-       <input class="text-sm cursor-pointer w-36 hidden" type="file" @change="loadExcelFile" />
-       <div class="text-white bg-blue font-bold rounded-lg text-sm px-2 py-2 text-center">
-         {{ csv || 'Import Excel' }}
-       </div>
-     </label>
-     <input type="hidden" v-model="csv" df-csv>
-</div>
- </template>
- <script lang="ts">
- import { defineComponent, onMounted, getCurrentInstance, ref,nextTick } from 'vue'
- import * as XLSX from 'xlsx';
- import * as fs from 'fs';
- import * as path from 'path';
- export default defineComponent({
-   setup() {
-     const el = ref(null) as any;
-     const nodeId = ref(0);
-     let df = null as any;
-     const csv = ref('');
-     const dataNode = ref({}) as any;
-     const internalInstance: any = getCurrentInstance();
-     df = internalInstance.appContext.config.globalProperties.$df.value;
-    function loadExcelFile(event){
-       csv.value = event.target.files[0].name;
-       nodeId.value = el.value?.parentElement.parentElement.id.slice(5)
-       dataNode.value = df.getNodeFromId(nodeId.value)
-       df.updateNodeDataFromId(nodeId.value, { mytemplate: "", csv: csv.value })
-       const file = event.target.files[0];
-       const reader = new FileReader();
-       reader.onload = (e) => {
-         const data = e.target?.result;
-         const workbook = XLSX.read(data, { type: 'binary' });
-         const sheet = workbook.Sheets[workbook.SheetNames[0]];
-         var columns: string[] = [];
-         var dataRows: any[] = [];
-         const rangen = sheet['!ref'];
-         if (rangen) {
-           const range = XLSX.utils.decode_range(rangen);
-           for (let C = range.s.c; C <= range.e.c; ++C) {
-             const cell = sheet[XLSX.utils.encode_cell({ r: range.s.r, c: C })];
-             const columnName: string = XLSX.utils.format_cell(cell);
-             columns.push(columnName);
-           }
-           for (let R = range.s.r + 1; R <= range.e.r; ++R) {
-             const row: any = {};
-             for (let C = range.s.c; C <= range.e.c; ++C) {
-               const cell = sheet[XLSX.utils.encode_cell({ r: R, c: C })];
-               const columnName: string = columns[C];
-               const cellValue: string = XLSX.utils.format_cell(cell);
-               row[columnName] = cellValue;
-             }
-             dataRows.push(row);
-           }
-         }
-         // Convert data to JSON format
-         const jsonData = JSON.stringify(dataRows, null, 2);
-         // Save JSON data to file
-         const outputFile = path.join('C:\\uploads\\', `${csv.value}`);
-         fs.writeFileSync(outputFile, jsonData);
-       };
-       reader.readAsBinaryString(file);
-     }
-     onMounted(async () => {
+  <div ref="el" class="input_field flex flex-col w-max mx-auto text-center">
+       <label >
+         <input class="text-sm cursor-pointer w-36 hidden" type="file" @change="loadExcelFile" />
+         <div class="text-white bg-blue font-bold rounded-lg text-sm px-2 py-2 text-center">
+           {{ csv || 'Import Excel' }}
+         </div>
+       </label>
+       <input type="hidden" v-model="csv" df-csv>
+  </div>
+  </template>
+  <script lang="ts">
+  import {  getCurrentInstance,nextTick } from 'vue'
+  import * as XLSX from 'xlsx';
+  import * as fs from 'fs';
+  import * as path from 'path';
+  export default {
+    name: 'ImportExcel',
+    data() {
+      return {
+        el:null as any,
+        nodeId:0,
+        df:null as any,
+        csv:'',
+        dataNode : {} as any, 
+      };
+    },
+    async mounted() { 
+      this.el=this.$refs.el;
+      const internalInstance: any = getCurrentInstance();
+      this.df = internalInstance.appContext.config.globalProperties.$df.value;
       await nextTick()
-       nodeId.value = el.value?.parentElement.parentElement.id.slice(5)
-       console.log(nodeId.value+"nodeId")
-        dataNode.value = df.getNodeFromId(nodeId.value)
-        csv.value = dataNode.value.data.csv;
-     }
-     );
- 
-     return {
-       el, csv,loadExcelFile
-     }
- 
-   }
- 
- })
- </script>
- <style>
- .nodeselect .vs__clear {
-   margin-bottom: 10px;
- }
- 
- .nodeselect .vs__open-indicator {
-   display: none;
- }
- </style>
- 
- 
+      this.nodeId = this.el?.parentElement.parentElement.id.slice(5)
+      console.log(this.nodeId+"nodeId")
+      this.dataNode = this.df.getNodeFromId(this.nodeId)
+      this.csv = this.dataNode.data.csv;
+    },
+    methods: {
+     loadExcelFile(event){
+         this.el=this.$refs.el;
+         this.csv = event.target.files[0].name;
+         this.nodeId = this.el?.parentElement.parentElement.id.slice(5)
+         this.dataNode = this.df.getNodeFromId(this.nodeId)
+         this.df.updateNodeDataFromId(this.nodeId, { mytemplate: "", csv: this.csv })
+         const file = event.target.files[0];
+         const reader = new FileReader();
+         reader.onload = (e) => {
+           const data = e.target?.result;
+           const workbook = XLSX.read(data, { type: 'binary' });
+           const sheet = workbook.Sheets[workbook.SheetNames[0]];
+           var columns: string[] = [];
+           var dataRows: any[] = [];
+           const rangen = sheet['!ref'];
+           if (rangen) {
+             const range = XLSX.utils.decode_range(rangen);
+             for (let C = range.s.c; C <= range.e.c; ++C) {
+               const cell = sheet[XLSX.utils.encode_cell({ r: range.s.r, c: C })];
+               const columnName: string = XLSX.utils.format_cell(cell);
+               columns.push(columnName);
+             }
+             for (let R = range.s.r + 1; R <= range.e.r; ++R) {
+               const row: any = {};
+               for (let C = range.s.c; C <= range.e.c; ++C) {
+                 const cell = sheet[XLSX.utils.encode_cell({ r: R, c: C })];
+                 const columnName: string = columns[C];
+                 const cellValue: string = XLSX.utils.format_cell(cell);
+                 row[columnName] = cellValue;
+               }
+               dataRows.push(row);
+             }
+           }
+           // Convert data to JSON format
+           const jsonData = JSON.stringify(dataRows, null, 2);
+           // Save JSON data to file
+           const outputFile = path.join('C:\\uploads\\', `${this.csv}`);
+           fs.writeFileSync(outputFile, jsonData);
+         };
+         reader.readAsBinaryString(file);
+       }
+    },
+  };
+  </script>
--- src/components/Node-file-input.vue
+++ src/components/Node-file-input.vue
@@ -1,57 +1,55 @@
 <template>
   <div ref="el" class="nodeselect">
-    <p id="node-title" className="text-sm">Search for a template</p>
-    <v-select for="mytemplate" style="color:#2d495c; 
-    width: 155px;" v-model="mytemplate" :options="options" @option:selected="onChangeFile()" df-mytemplate />
+     <p id="node-title" className="text-sm">Search for a template</p>
+     <v-select for="mytemplate" style="color:#2d495c; 
+     width: 155px;" v-model="mytemplate" :options="options" @option:selected="onChangeFile()" df-mytemplate />
   </div>
-</template>
-<script lang="ts">
-import { defineComponent, onMounted, getCurrentInstance, ref } from 'vue'
-import { ipcRenderer } from 'electron';
-export default defineComponent({
-  setup() {
-    const el = ref(null) as any;
-    const nodeId = ref(0);
-    let df = null as any;
-    const mytemplate = ref('nothing');
-    const dataNode = ref({}) as any;
-    const outputnodeId = ref(0);
-    const options = ref([
-    ]);
-    const internalInstance: any = getCurrentInstance();
-    df = internalInstance.appContext.config.globalProperties.$df.value;
-    async function onChangeFile() {
-      nodeId.value = el.value?.parentElement.parentElement.id.slice(5)
-      dataNode.value = df.getNodeFromId(nodeId.value)
-      df.updateNodeDataFromId(nodeId.value, { mytemplate: mytemplate.value, csv: "" })
-      if (dataNode.value.outputs.output_1.connections[0]?.node) {
-        outputnodeId.value = parseFloat(dataNode.value.outputs.output_1.connections[0].node)
-        df.updateNodeDataFromId(outputnodeId.value, { mytemplate: mytemplate.value, csv: "" })
-      }
-    }
-    onMounted(async () => {
-      options.value = await ipcRenderer.invoke('getQuillContentName');
-      nodeId.value = el.value?.parentElement.parentElement.id.slice(5)
-      dataNode.value = df.getNodeFromId(nodeId.value)
-      mytemplate.value = dataNode.value.data.mytemplate;
-    }
-
-    );
-
-    return {
-      el, mytemplate, options, onChangeFile
-    }
-
+ </template>
+ <script lang="ts">
+ import { getCurrentInstance  } from 'vue'
+ import { ipcRenderer } from 'electron';
+ 
+ export default {
+   name: 'SelectSearch',
+   data() {
+     return {
+       el:null as any,
+       nodeId:0,
+       outputnodeId:0,
+       df:null as any,
+       mytemplate:'nothing',
+       dataNode : {} as any, 
+       options : []
+     };
+   },
+  async mounted() { 
+     this.el=this.$refs.el;
+     const internalInstance: any = getCurrentInstance();
+     this.df = internalInstance.appContext.config.globalProperties.$df.value;
+     this.options = await ipcRenderer.invoke('getQuillContentName');
+     this.nodeId = this.el?.parentElement.parentElement.id.slice(5)
+     this.dataNode = this.df.getNodeFromId(this.nodeId)
+     this.mytemplate = this.dataNode.data.mytemplate;
+   },
+   methods: {
+    async onChangeFile() {
+       this.nodeId = this.el?.parentElement.parentElement.id.slice(5)
+       this.dataNode = this.df.getNodeFromId(this.nodeId)
+       this.df.updateNodeDataFromId(this.nodeId, { mytemplate: this.mytemplate, csv: "" })
+       if (this.dataNode.outputs.output_1.connections[0]?.node) { 
+       this.outputnodeId = parseFloat(this.dataNode.outputs.output_1.connections[0].node)
+       this.df.updateNodeDataFromId(this.outputnodeId, { mytemplate: this.mytemplate, csv: "" })
+       }
+    }  
+   },
+ };
+ </script>
+  <style>
+  .nodeselect .vs__clear {
+    margin-bottom: 10px;
   }
-
-})
-</script>
-<style>
-.nodeselect .vs__clear {
-  margin-bottom: 10px;
-}
-
-.nodeselect .vs__open-indicator {
-  display: none;
-}
-</style>
+  
+  .nodeselect .vs__open-indicator {
+    display: none;
+  }
+  </style>
