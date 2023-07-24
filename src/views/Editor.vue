50ccd9a7b4b52271d73f8347e9ba3b3f4341b2ff
SANAE LMNJAOUI <sanaelmnjaoui12@gmail.com>
2023-07-24 15:03:00 +0000

    style changed

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -57,7 +57,7 @@
                             <span class="material-icons">keyboard_double_arrow_left</span>
                         </button>
                         <h4 className="border-b-4 p-2 border-white text-center font-bold text-black-700 mt-0.1 ">File History</h4>
-
+                      <br>
                         <div id="app" class="scroll-container">
                             <div>
                                 <div style="display: flex">
@@ -84,59 +84,57 @@
                         </div>
               </aside>
               <div className="flex flex-col gap-2 w-[300px] mx-auto mr-0.1 h-full">
-                <label>
-                  <input class="text-sm cursor-pointer w-36 hidden" type="file" @input="importDocument" accept=".doc, .docx">
-                  <div class="btn  mr-2 mb-2 w-[240px] cursor-pointer flex items-center justify-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                      class="mr-2 bi bi-cloud-arrow-up-fill" viewBox="0 0 16 16">
-                      <path
-                        d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2zm2.354 5.146a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707L6.354 7.854a.5.5 0 1 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2z" />
-                    </svg>
-                    {{ t("editor.open") }}
-                  </div>
-                </label>
-                <button class="btn mr-3 w-[240px] flex items-center justify-center" @click="downloadPdf">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                    class="mr-2 bi bi-cloud-arrow-down-fill" viewBox="0 0 16 16">
-                    <path
-                      d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2zm2.354 6.854-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5a.5.5 0 0 1 1 0v3.793l1.146-1.147a.5.5 0 0 1 .708.708z" />
-                  </svg>
-                  {{ t("editor.generate") }}
-                </button>
-                <div className="relative w-[240px] mx-auto mr-4">
-                <div className="relative w-[240px] mx-auto mr-4">
-                      <button class="menu-toggle absolute left-0 top-0" @click="showSidebar = !showSidebar">
-                          <span class="material-icons">keyboard_double_arrow_right</span>
-                      </button>
-                      <h2 className="border-b-4 p-2 border-primary-dark text-center font-bold text-black-700  ">Node Excel</h2>
-                </div>
-                <div class="file_upload p-1 relative border-4 border-dotted border-primary-dark rounded-lg"
-                  style="width: 210px; height: 130px; margin-left: 5%;">
-                  <svg class="text-primary-dark w-24 mx-auto mb-2" style="width: 100px; height: 70px;"
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
-                  </svg>
-                  <div class="input_field flex flex-col w-max mx-auto text-center">
                     <label>
-                      <input class="text-sm cursor-pointer w-36 hidden" type="file" @change="loadExcelFile" />
-                      <div class="btn text-sm rounded-lg text-center m-auto" style="height: unset; width: 190px;">
-                        {{ t("editor.select") }}</div>
+                      <input class="text-sm cursor-pointer w-36 hidden" type="file" @input="importDocument" accept=".doc, .docx">
+                      <div class="btn  mr-2 mb-2 w-[240px] cursor-pointer flex items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                          class="mr-2 bi bi-cloud-arrow-up-fill" viewBox="0 0 16 16">
+                          <path
+                            d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2zm2.354 5.146a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707L6.354 7.854a.5.5 0 1 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2z" />
+                        </svg>
+                        {{ t("editor.open") }}
+                      </div>
                     </label>
-                  </div>
-                </div>
-                <div v-if="allow" class="scroll-container1">
-        <tree  class="cursor-grab" :nodes="nodes" :config="config"></tree>
-      </div>
+                    <button class="btn mr-3 w-[240px] flex items-center justify-center" @click="downloadPdf">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="mr-2 bi bi-cloud-arrow-down-fill" viewBox="0 0 16 16">
+                        <path
+                          d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2zm2.354 6.854-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5a.5.5 0 0 1 1 0v3.793l1.146-1.147a.5.5 0 0 1 .708.708z" />
+                      </svg>
+                      {{ t("editor.generate") }}
+                    </button>
+                    <div className="relative w-[240px] mx-auto mr-4">
+                          <button class="menu-toggle absolute left-0 top-0" @click="showSidebar = !showSidebar">
+                              <span class="material-icons">keyboard_double_arrow_right</span>
+                          </button>
+                          <h2 className="border-b-4 p-2 border-primary-dark text-center font-bold text-black-700  ">Node Excel</h2>
+                    </div>
+                    <div class="file_upload p-1 relative border-4 border-dotted border-primary-dark rounded-lg"
+                      style="width: 210px; height: 130px; margin-left: 5%;">
+                      <svg class="text-primary-dark w-24 mx-auto mb-2" style="width: 100px; height: 70px;"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+                      </svg>
+                      <div class="input_field flex flex-col w-max mx-auto text-center">
+                        <label>
+                          <input class="text-sm cursor-pointer w-36 hidden" type="file" @change="loadExcelFile" />
+                          <div class="btn text-sm rounded-lg text-center m-auto" style="height: unset; width: 190px;">
+                            {{ t("editor.select") }}</div>
+                        </label>
+                      </div>
+                    </div>
+                    <div v-if="allow" class="scroll-container1">
+                      <tree  class="cursor-grab" :nodes="nodes" :config="config"></tree>
+                    </div>
               </div>
               <div className="flex flex-col w-full h-full">
-                <div id="editor" ref="editor">
-                </div>
+                    <div id="editor" ref="editor">
+                    </div>
               </div>
       </div>
   
   </div>
-</div>
 </template>
 
 <script lang="ts">
